3f339f55c31f44fd37f8164bde475a56c326b364
Ehsan Barooni <barooni.eh@gmail.com>
2022-06-03 10:36:38 +0200

    Renamed material.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MdBootstrapModule} from "../md-bootstrap/material.module";
+import {MdBootstrapModule} from "../md-bootstrap/md-bootstrap.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
