d511e640f2525263d385e70ccd9e7a3272e530cd
Ehsan Barooni <barooni.eh@gmail.com>
2022-06-06 22:12:40 +0200

    Added footer and navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MdBootstrapModule} from "../md-bootstrap/md-bootstrap.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
